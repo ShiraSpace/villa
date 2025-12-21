867e3832eccbcbf88f026c5323dcf9f7dab4768f
ShiraSpace <shira.navama@gmail.com>
2025-12-21 22:32:27 +0700

    update header

--- src/components/navigation/Header.tsx
+++ src/components/navigation/Header.tsx
@@ -5,6 +5,7 @@ import { useScrollPosition } from '@/hooks/useScrollPosition';
 import { Logo } from '@/components/navigation/Logo';
 import { ContactButton } from '@/components/navigation/ContactButton';
 import { Menu } from '@/components/navigation/Menu';
+import { TEST_ID } from '@/components/navigation/constants';
 
 export function Header(): JSX.Element {
   const scrollPosition = useScrollPosition();
@@ -16,6 +17,7 @@ export function Header(): JSX.Element {
 
   return (
     <header
+      data-testid={TEST_ID.HEADER}
       className={`fixed top-0 left-0 right-0 z-50 transition-all duration-300 ${scrollClasses}`}
     >
       <div className="container-custom">
